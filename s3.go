03958c811ae44dc4362427a34c32228af0c358ed
tagasouichiro <stitow14@gmail.com>
2018-08-28 00:11:04 +0900

    Add batch read

--- s3.go
+++ s3.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -41,14 +42,25 @@ func (s *blobs3) ReadWrite(w io.Writer) error {
 
 	var r = region
 	sess := session.Must(session.NewSession(&aws.Config{Region: &r}))
-	downloader := s3manager.NewDownloader(sess)
-	obj := &s3.GetObjectInput{
-		Bucket: aws.String(s.bucket),
-		Key:    aws.String(s.key),
+	result, err := listObjects(s.bucket, s.key, sess)
+	if err != nil {
+		return err
+	}
+	batchInputs := make([]s3manager.BatchDownloadObject, *result.KeyCount)
+	for i, v := range result.Contents {
+		batchInputs[i] = s3manager.BatchDownloadObject{
+			Object: &s3.GetObjectInput{
+				Bucket: aws.String(s.bucket),
+				Key:    v.Key,
+			},
+			Writer: bufAt,
+		}
 	}
 
-	err := download(obj, bufAt, sess, downloader)
-	if err != nil {
+	iter := &s3manager.DownloadObjectsIterator{Objects: batchInputs}
+	downloader := s3manager.NewDownloader(sess)
+
+	if err := downloader.DownloadWithIterator(aws.BackgroundContext(), iter); err != nil {
 		return errors.Wrap(err, "download error")
 	}
 
@@ -67,6 +79,30 @@ func (s *blobs3) ReadWrite(w io.Writer) error {
 	}
 
 	return nil
+}
+
+func listObjects(bucket, key string, sess *session.Session) (*s3.ListObjectsV2Output, error) {
+	svc := s3.New(sess)
+	input := &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(key),
+	}
+
+	result, err := svc.ListObjectsV2(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchBucket:
+				return nil, fmt.Errorf("%v: %v", s3.ErrCodeNoSuchBucket, aerr)
+			default:
+				return nil, aerr
+			}
+		} else {
+			return nil, err
+		}
+		return nil, err
+	}
+	return result, nil
 }
 
 func download(obj *s3.GetObjectInput, w io.WriterAt, sess *session.Session, downloader *s3manager.Downloader) error {
